a1bfbe62e5101aad23a9419935a036af71feb5f3
SABloger <sabloger@gmail.com>
2022-05-26 15:59:09 +0430

    Test Sitemap.WriteTo

--- smg/sitemap_test.go
+++ smg/sitemap_test.go
@@ -1,31 +1,33 @@
 package smg
 
 import (
+	"bytes"
 	"encoding/xml"
 	"fmt"
 	"io/ioutil"
 	"os"
 	"testing"
 	"time"
+
+	"github.com/stretchr/testify/assert"
 )
 
 type UrlSet struct {
-	XMLName xml.Name `xml:"urlset"`
-	Urls []UrlData `xml:"url"`
+	XMLName xml.Name  `xml:"urlset"`
+	Urls    []UrlData `xml:"url"`
 }
 
 type UrlData struct {
-	XMLName xml.Name `xml:"url"`
-	Loc string `xml:"loc"`
-	LasMod string `xml:"lastmod"`
-	ChangeFreq string `xml:"changefreq"`
-	Priority string `xml:"priority"`
+	XMLName    xml.Name `xml:"url"`
+	Loc        string   `xml:"loc"`
+	LasMod     string   `xml:"lastmod"`
+	ChangeFreq string   `xml:"changefreq"`
+	Priority   string   `xml:"priority"`
 }
-
 
 // TestSingleSitemap tests the module against Single-file sitemap usage format.
 func TestSingleSitemap(t *testing.T) {
-	path := getNewPath()
+	path := t.TempDir()
 	now := time.Now().UTC()
 	routes := buildRoutes(10, 40, 10)
 
@@ -67,16 +69,14 @@ func TestSingleSitemap(t *testing.T) {
 	for _, filename := range filenames {
 		assertOutputFile(t, path, filename)
 	}
-	// -----------------------------------------------------------------
-	// Removing the generated path and files
-	removeTmpFiles(t, path)
 }
 
 // TestSitemapAdd tests that the Add function produces a proper URL
 func TestSitemapAdd(t *testing.T) {
-	path := "./tmp/sitemap_test"
+	path := t.TempDir()
 	testLocation := "/test"
 	now := time.Now().UTC()
+
 	sm := NewSitemap(true)
 	sm.SetName("single_sitemap")
 	sm.SetHostname(baseURL)
@@ -98,7 +98,8 @@ func TestSitemapAdd(t *testing.T) {
 	if err != nil {
 		t.Fatal("Unable to Save Sitemap:", err)
 	}
-	xmlFile, err := os.Open(fmt.Sprintf("%s/%s",path, filepath[0]))
+
+	xmlFile, err := os.Open(fmt.Sprintf("%s/%s", path, filepath[0]))
 	if err != nil {
 		t.Fatal("Unable to open file:", err)
 	}
@@ -110,10 +111,46 @@ func TestSitemapAdd(t *testing.T) {
 		t.Fatal("Unable to unmarhsall sitemap byte array into xml: ", err)
 	}
 	actualUrl := urlSet.Urls[0].Loc
-	if actualUrl != expectedUrl {
-		t.Fatal(fmt.Sprintf("URL Mismatch: \nActual: %s\nExpected: %s", actualUrl, expectedUrl))
+	assert.Equal(t, expectedUrl, actualUrl)
+}
+
+func TestWriteTo(t *testing.T) {
+	path := t.TempDir()
+	now := time.Now().UTC()
+	testLocation := "/test"
+
+	sm := NewSitemap(true)
+	sm.SetHostname(baseURL)
+	sm.SetLastMod(&now)
+	sm.SetOutputPath(path)
+	sm.SetCompress(false)
+
+	err := sm.Add(&SitemapLoc{
+		Loc:        testLocation,
+		LastMod:    &now,
+		ChangeFreq: Always,
+		Priority:   0.4,
+	})
+	if err != nil {
+		t.Fatal("Unable to add SitemapLoc:", err)
 	}
+	sm.Finalize()
 
-	removeTmpFiles(t, "./tmp")
+	//----- Write to buffer
 
+	buf := bytes.Buffer{}
+	_, err = sm.WriteTo(&buf)
+	if err != nil {
+		t.Fatal("Unable to write to buffer:", err)
+	}
+	//-----
+	expectedUrl := fmt.Sprintf("%s%s", baseURL, testLocation)
+
+	var urlSet UrlSet
+	err = xml.Unmarshal(buf.Bytes(), &urlSet)
+	if err != nil {
+		t.Fatal("Unable to unmarhsall sitemap byte array into xml: ", err)
+	}
+	actualUrl := urlSet.Urls[0].Loc
+	assert.Equal(t, expectedUrl, actualUrl)
 }
